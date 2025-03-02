b3ed7338c52c15d5f4bab234707ae7b65ae3c161
Ola Joseph <oaj3@student.le.ac.uk>
2025-03-02 00:29:53 +0000

    Add initial project files

---
+++ src/test/java/uk/ac/le/soa/activityservice/ActivityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.le.soa.activityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
