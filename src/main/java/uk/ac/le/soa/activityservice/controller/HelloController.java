a1767bcdaf0ad8a1ee0d6b4358a11a749c598228
sjds2 <sjds2@student.le.ac.uk>
2025-03-04 15:56:37 +0000

    Added a hello controller for testing

---
+++ src/main/java/uk/ac/le/soa/activityservice/controller/HelloController.java
@@ -0,0 +1,17 @@
+package uk.ac.le.soa.activityservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class HelloController {
+
+
+    @GetMapping("")
+    public String hello(){
+        return "Hello";
+    }
+
+}
