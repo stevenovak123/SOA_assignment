0ce015d0d8de7ddd4a260aac2040be0d7525735f
sjds2 <sjds2@student.le.ac.uk>
2025-03-08 03:12:47 +0000

    Added by default Database initialization of data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,31 @@
+INSERT INTO soa.city (id, city, latitude, longitude) VALUES ('1', 'London', '51.507351', '-0.127758');
+INSERT INTO soa.city (id, city, latitude, longitude) VALUES ('2', 'Leicester', '52.636879', '-1.139759');
+INSERT INTO soa.city (id, city, latitude, longitude) VALUES ('3', 'Manchester', '53.480759', '-2.242631');
+INSERT INTO soa.city (id, city, latitude, longitude) VALUES ('4', 'Liverpool', '53.408371', '-2.2991573');
+INSERT INTO soa.city (id, city, latitude, longitude) VALUES ('5', 'Brighton', '36.063168', '-95.881668');
+
+
+INSERT INTO soa.activity (id, description, name, type) VALUES ('1', 'In national parks and nature reserves.', 'Bushwalking', 'Outdoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('On trails in mountains and parks.', 'Hiking', 'Outdoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('In limestone caves and caverns.', 'Cave Tours ', 'Outdoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('On cliffs, rocks.', 'Rock Climbing & Abseiling', 'Outdoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('In campgrounds, forests, and parks.', 'Camping ', 'Outdoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('On bike paths, roads, and trails.', 'Cycling', 'Outdoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('In rivers, lakes, and oceans.', 'Canoeing & Kayaking', 'Outdoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('In rivers, lakes, and coastal waters.', 'Fishing', 'Outdoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('For playing board games with friends.', 'Boardgame House', 'Indoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('At cultural and art museums.', 'Museum Visiting', 'Indoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('At gaming arcades with various machines.', 'Arcade', 'Indoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('At indoor climbing gyms or outdoor rock formations.', 'Bouldering', 'Indoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('In themed puzzle-solving rooms.', 'Escape Room ', 'Indoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('At licensed shooting ranges.', 'Shooting Range', 'Indoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('For live stage performances and plays.', 'Theatre', 'Indoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('At cinemas or movie theaters', 'Movies', 'Indoor');
+INSERT INTO soa.activity (description, name, type) VALUES (' In sports bars or public viewing areas.', 'Sports Screening', 'Indoor');
+INSERT INTO soa.activity (description, name, type) VALUES ('At karaoke bars or private karaoke rooms.', 'Karaoke', 'Indoor');
+
+INSERT INTO soa.route_data (distance, estimated_time, route_description) VALUES ('14.47', '131', 'Drive');
+INSERT INTO soa.route_data (distance, estimated_time, route_description) VALUES ('12.5', '160', 'Ferry');
+INSERT INTO soa.route_data (distance, estimated_time, route_description) VALUES ('20.2', '120', 'Train');
+INSERT INTO soa.route_data (distance, estimated_time, route_description) VALUES ('10.8', '90', 'Bus');
+INSERT INTO soa.route_data (distance, estimated_time, route_description) VALUES ('15.7', '180', 'Drive');
